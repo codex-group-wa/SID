240927f878fa56504ba31db50b703cb2c56e1954
Declan Wade <17171018+declan-wade@users.noreply.github.com>
2025-08-13 09:00:05 +0800

    Further rework

--- next.config.ts
+++ next.config.ts
@@ -3,14 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "standalone",
   pageExtensions: ["ts", "tsx"],
-  publicRuntimeConfig: {
-    REPO_URL: process.env.REPO_URL,
-    SID_ALLOWED_HOSTS: process.env.SID_ALLOWED_HOSTS,
-    REPO_NAME: process.env.REPO_NAME,
-    GITHUB_WEBHOOK_SECRET: process.env.GITHUB_WEBHOOK_SECRET,
-    WORKING_DIR: process.env.WORKING_DIR,
-    NTFY_URL: process.env.NTFY_URL,
-  },
 };
 
 export default nextConfig;
